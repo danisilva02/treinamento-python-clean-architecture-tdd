51a7cb0417cb39efd44a64f6be8011aea00df8c0
dsilva <daniel.silvamiranda02@gmail.com>
2026-02-10 12:59:36 -0300

    fix: update docs

--- src/main.py
+++ src/main.py
@@ -18,9 +18,9 @@ def lifespan(app: FastAPI):
     return app
 
 app = FastAPI(
-    title="Treinamento Python Clean Architecture + TDD",
+    title="Treinamento Clean Architecture + TDD",
     description="Crud de usuário, categoria e produto",
-    summary="API para o Treinamento Python Clean Architecture + TDD 🚀",
+    summary="API para o Treinamento Clean Architecture + TDD 🚀",
     version="0.0.1",
     root_path="/api",
     terms_of_service="https://areteacademy.com.br",
